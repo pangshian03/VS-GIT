41141108d2c29db7571d5b1fb6838ce1bd87a9f6
shi an <d200142b@sc.edu.my>
2021-07-07 15:34:11 +0800

    init

--- cart/resources/views/layout.blade.php
+++ cart/resources/views/layout.blade.php
@@ -51,7 +51,7 @@
 @yield('content')
 
 <!--Footer-->
-    <footer class="bg-dark text-center text-white">
+    <footer class="bg-dark text-white">
 
       <!-- Grid container -->
       <div class="container p-4">
@@ -106,20 +106,30 @@
 
             <!--Grid column-->
             <div class="col-lg-3 col-md-3 mb-4 mb-md-0">
-              <h5 class="text-uppercase">Links</h5>
+              <h5 class="text-uppercase">Company name</h5>
+              <p>
+                Here you can use rows and columns to organize your footer content. Lorem ipsum
+                dolor sit amet, consectetur adipisicing elit.
+              </p>
+            </div>
+            <!--/Grid column-->
+
+            <!--Grid column-->
+            <div class="col-lg-3 col-md-3 mb-4 mb-md-0">
+              <h5 class="text-uppercase">Products</h5>
 
               <ul class="list-unstyled mb-0">
                 <li>
-                  <a href="#!" class="text-white">Link 1</a>
+                  <a href="#!" class="text-white">Laravel</a>
                 </li>
                 <li>
-                  <a href="#!" class="text-white">Link 2</a>
+                  <a href="#!" class="text-white">Rect</a>
                 </li>
                 <li>
-                  <a href="#!" class="text-white">Link 3</a>
+                  <a href="#!" class="text-white">Angular</a>
                 </li>
                 <li>
-                  <a href="#!" class="text-white">Link 4</a>
+                  <a href="#!" class="text-white">Vue</a>
                 </li>
               </ul>
             </div>
@@ -127,20 +137,20 @@
 
             <!--Grid column-->
             <div class="col-lg-3 col-md-3 mb-4 mb-md-0">
-              <h5 class="text-uppercase">Links</h5>
+              <h5 class="text-uppercase">useful link</h5>
 
               <ul class="list-unstyled mb-0">
                 <li>
-                  <a href="#!" class="text-white">Link 1</a>
+                  <a href="#!" class="text-white">Pricing</a>
                 </li>
                 <li>
-                  <a href="#!" class="text-white">Link 2</a>
+                  <a href="#!" class="text-white">Orders</a>
                 </li>
                 <li>
-                  <a href="#!" class="text-white">Link 3</a>
+                  <a href="#!" class="text-white">Setting</a>
                 </li>
                 <li>
-                  <a href="#!" class="text-white">Link 4</a>
+                  <a href="#!" class="text-white">Help</a>
                 </li>
               </ul>
             </div>
@@ -148,41 +158,37 @@
 
             <!--Grid column-->
             <div class="col-lg-3 col-md-3 mb-4 mb-md-0">
-              <h5 class="text-uppercase">Links</h5>
+              <h5 class="text-uppercase">Contact</h5>
 
               <ul class="list-unstyled mb-0">
                 <li>
-                  <a href="#!" class="text-white">Link 1</a>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="30" height="18" fill="currentColor" class="bi bi-house-fill" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293l6-6zm5-.793V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
+                    <path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/>
+                  </svg>
+                  <span>New York, NY 10012, US</span>
                 </li>
-                <li>
-                  <a href="#!" class="text-white">Link 2</a>
-                </li>
-                <li>
-                  <a href="#!" class="text-white">Link 3</a>
-                </li>
-                <li>
-                  <a href="#!" class="text-white">Link 4</a>
-                </li>
-              </ul>
-            </div>
-            <!--/Grid column-->
 
-            <!--Grid column-->
-            <div class="col-lg-3 col-md-3 mb-4 mb-md-0">
-              <h5 class="text-uppercase">Links</h5>
+                <li>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="18" fill="currentColor" class="bi bi-envelope-fill" viewBox="0 0 16 16">
+                    <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555zM0 4.697v7.104l5.803-3.558L0 4.697zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757zm3.436-.586L16 11.801V4.697l-5.803 3.546z"/>
+                    </svg>
+                    info@example.com
+                </li>
 
-              <ul class="list-unstyled mb-0">
                 <li>
-                  <a href="#!" class="text-white">Link 1</a>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="30" height="18" fill="currentColor" class="bi bi-telephone-fill" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
+                  </svg>
+                  + 01 234 567 89
                 </li>
+                
                 <li>
-                  <a href="#!" class="text-white">Link 2</a>
-                </li>
-                <li>
-                  <a href="#!" class="text-white">Link 3</a>
-                </li>
-                <li>
-                  <a href="#!" class="text-white">Link 4</a>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="30" height="18" fill="currentColor" class="bi bi-printer-fill" viewBox="0 0 16 16">
+                    <path d="M5 1a2 2 0 0 0-2 2v1h10V3a2 2 0 0 0-2-2H5zm6 8H5a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1z"/>
+                    <path d="M0 7a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2h-1v-2a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v2H2a2 2 0 0 1-2-2V7zm2.5 1a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z"/>
+                  </svg>
+                  + 01 234 567 99
                 </li>
               </ul>
             </div>
